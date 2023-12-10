c98119b50a8cae9e8fbf2c4b779171bae0428183
Hans Hoffman <hans@oberan.dev>
2023-12-10 11:50:40 -0500

    chore: update nix to release-23.11

--- shell.nix
+++ shell.nix
@@ -2,9 +2,5 @@ let
   sources = import ./nix/sources.nix { };
   pkgs = import sources.nixpkgs { };
 in pkgs.mkShell {
-  buildInputs = [
-    pkgs.git
-    pkgs.nixfmt
-    pkgs.nodejs_20
-  ];
+  buildInputs = [ pkgs.nixfmt pkgs.nodejs_20 pkgs.nodePackages.pnpm ];
 }
